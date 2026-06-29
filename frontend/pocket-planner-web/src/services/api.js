2bad6c2a3d507fd40566848860c8677142aef175
Sri Lakshmi K <srilakshmik1810@gmail.com>
2026-06-29 12:00:21 +0530

    Fix API base URL

--- frontend/pocket-planner-web/src/services/api.js
+++ frontend/pocket-planner-web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backup-1-ncmr.onrender.com",
+  baseURL: "https://backup-1-ncmr.onrender.com/api",
 });
 
 export default api;
