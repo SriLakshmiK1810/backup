2d072143c8997cb449220e703f9ebe0c90d4e2e7
Sri Lakshmi K <srilakshmik1810@gmail.com>
2026-06-29 11:41:31 +0530

    Update CORS

--- backend/src/main/java/com/pocketplanner/controller/UserController.java
+++ backend/src/main/java/com/pocketplanner/controller/UserController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {
+    "http://localhost:3000",
+    "https://backup-kappa-two.vercel.app"
+})
 public class UserController {
 
     @Autowired
